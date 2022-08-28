d43e62403fabc912c995a1f09597588ed080e91f
tmaz0003 <tmaz0003@student.monash.edu>
2022-08-29 00:46:30 +0200

    Complete

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,33 @@
-
-const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const User = require("../models/User").default;
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
  
-const User = require("../models/User");
 
 const router = require("express").Router();
 
-router.post("userrposttest", (req,res)=>{
-    const username = req.body.username
-    console.log(username);
 
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req,res)=>{
+   if(req.body.password){
+    req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.PASS_SEC
+    ).toString();
+   }
+   
+    try{
+        const udatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set: req.body,
+        },
+        { new: true }
+        );
+        res.status(200).json(udatedUser)
+    }catch(err){
+        res.status(500).json(err);
+    }
 });
 
 //DELETE   
-
 router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=>{
     try{
         await User.findByIdAndDelete(req.params.id)
@@ -71,7 +85,7 @@ router.get("/stats", verifyTokenAndAdmin, async (req,res) => {
                 }
             }
         ]);
-
+        res.status(200).json(data)
     }catch(err){
         res.status(500).json(err);
     }
