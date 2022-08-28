e4497424bf330437425834eb06f3af194573efba
tmaz0003 <tmaz0003@student.monash.edu>
2022-08-28 02:44:37 +0200

    CRUD part 1

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,36 @@
+
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+ 
 const router = require("express").Router();
 
-//router.put("/:id", verifyToken)
+//Update
+router.put("/:id", verifyTokenAndAuthorization, async(req, res) =>{
+     if (req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+        ).toString();
+     }
+
+     try{
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id, 
+            {
+            $set: req.body,
+        },
+        {new: true }
+     );
+     res.status(200).json(updatedUser);
+     } catch (err) {
+        res.status(500).json(err);
+     }
+});
 
 router.post("userrposttest", (req,res)=>{
     const username = req.body.username
     console.log(username);
+
 });
 
 module.exports = router
